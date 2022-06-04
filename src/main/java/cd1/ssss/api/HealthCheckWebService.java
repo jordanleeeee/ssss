5bbeec8bd7454a153f8d2a6cf5652b1077ea1090
JordanLee <jordanlee80@gmail.com>
2022-06-04 18:53:43 +0800

    add health check

---
+++ src/main/java/cd1/ssss/api/HealthCheckWebService.java
@@ -0,0 +1,16 @@
+package cd1.ssss.api;
+
+import cd1.ssss.service.Network;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Jordan
+ */
+@RestController
+public class HealthCheckWebService {
+    @GetMapping("/ping")
+    String ping(){
+        return "pong: " + Network.LOCAL_HOST_NAME;
+    }
+}
---
+++ src/main/java/cd1/ssss/service/Network.java
@@ -0,0 +1,22 @@
+package cd1.ssss.service;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * @author Jordan
+ */
+public class Network {
+    public static final String LOCAL_HOST_ADDRESS;
+    public static final String LOCAL_HOST_NAME;
+
+    static {
+        try {
+            InetAddress address = InetAddress.getLocalHost();
+            LOCAL_HOST_ADDRESS = address.getHostAddress();
+            LOCAL_HOST_NAME = address.getHostName();
+        } catch (UnknownHostException e) {
+            throw new Error(e);
+        }
+    }
+}
